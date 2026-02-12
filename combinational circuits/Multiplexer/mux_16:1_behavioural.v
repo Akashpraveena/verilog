d226de22532a944f118dcf00b6593ecde3046106
Akashpraveena <130641076+Akashpraveena@users.noreply.github.com>
2026-02-12 12:59:30 +0530

    Create mux_16:1_behavioural.v

---
+++ combinational circuits/Multiplexer/mux_16:1_behavioural.v
@@ -0,0 +1,23 @@
+module mux16_1( input[15:0]d, input[3:0]s,output reg y);
+  always@(*)begin
+    case (s)
+      4'b0000: y=d[0];
+      4'b0001: y=d[1];
+      4'b0010: y=d[2];
+      4'b0011: y=d[3];
+      4'b0100: y=d[4];
+      4'b0101: y=d[5];
+      4'b0110: y=d[6];
+      4'b0111: y=d[7];
+      4'b1000: y=d[8];
+      4'b1001: y=d[9];
+      4'b1010: y=d[10];
+      4'b1011: y=d[11];
+      4'b1100: y=d[12];
+      4'b1101: y=d[13];
+      4'b1110: y=d[14];
+      4'b1111: y=d[15];
+      default: y=1'b0;
+    endcase
+  end
+endmodule
